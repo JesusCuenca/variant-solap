acfee4c086defd87b34fdfba7eb1804cd5a4c214
Jesús Cuenca López <jcuenca@bitban.com>
2023-11-05 21:14:41 +0100

    wip: removed unnecessary file.

--- dist/commands/crh2gene.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.chr2gene = void 0;
-exports.chr2gene = {
-    command: 'chr2gene <chromosome-id> <files..>',
-    aliases: ['cg', 'c2g'],
-    handler(argv) {
-    }
-};
